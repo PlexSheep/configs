8dae6a7403ef11dfe7b66d04c13e26d3fdd072a9
PlexSheep <PlexSheep@protonmail.com>
2023-01-07 23:30:33 +0100

    whatever zsh updates i made

--- cpConfigsToTarget.sh
+++ cpConfigsToTarget.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 mkdir -p /etc/xdg
-mkdir -p /etc/
 mkdir -p /home/
 mkdir -p /usr/share
 
-cp -fr ./home/.wakeonlan ~
 cp -fr ./etc/vimrc /etc/vimrc
-cp -fr ./etc/xdg/nvim /etc/xdg/nvim/
-cp -fr ./home/.zsh ~
-cp -fr ./home/.zsh-server ~
-ln ~/.zsh/zshrc ~/.zshrc
-cp -fr .home/.p10k.zsh ~
-cp -fr .home/.gitconfig ~
+cp -fr ./home/.* ~
+cp -fr ./home/* ~
 cp -fr ./usr/share/* /usr/share/
 cp -fr ./etc/* /etc
+
+ln ~/.zsh/zshrc ~/.zshrc
--- cpConfigsToTargetSERVER.sh
+++ cpConfigsToTargetSERVER.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mkdir -p /etc/xdg
-mkdir -p /etc/
 mkdir -p /home/
 mkdir -p /usr/share
 
-cp -fr ./etc/vimrc /etc/vimrc
+cp -fr ./etc/* /etc/
 cp -fr ./home/.zsh-server ~
-ln ~/.zsh-server/zshrc ~/.zshrc
 cp -fr ./usr/share/* /usr/share/
 cp -fr ./etc/* /etc
+
+ln ~/.zsh-server/zshrc ~/.zshrc
