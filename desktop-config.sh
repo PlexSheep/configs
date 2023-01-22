fa172cee05fd428f9303b9a75dc44b0e4ce87003
PlexSheep <PlexSheep@protonmail.com>
2023-01-23 00:46:03 +0100

    important script fix

--- desktop-config.sh
+++ desktop-config.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-cp -r $(pwd)/home/.gitconfig ~
-cp -r $(pwd)/home/.p10k.zsh ~
-cp -r $(pwd)/home/.zsh ~
-cp -r $(pwd)/home/.wakeonlan ~
-cp -r $(pwd)/home/.vimrc ~
-cp -r $(pwd)/home/.local ~
-cp -r $(pwd)/home/.config ~
-cp -r $(pwd)/home/.tmux.conf ~
 
 rm ~/.zsh-very-old
 mv ~/.zsh-old ~/.zsh-very-old
@@ -15,6 +7,15 @@ mv ~/.zsh ~/.zsh-old
 rm ~/.zshrc-very-old
 mv ~/.zshrc-old ~/.zshrc-very-old
 mv ~/.zshrc ~/.zshrc-old
+
+cp -r $(pwd)/home/.gitconfig ~
+cp -r $(pwd)/home/.p10k.zsh ~
+cp -r $(pwd)/home/.zsh ~
+cp -r $(pwd)/home/.wakeonlan ~
+cp -r $(pwd)/home/.vimrc ~
+cp -r $(pwd)/home/.local ~
+cp -r $(pwd)/home/.config ~
+cp -r $(pwd)/home/.tmux.conf ~
 
 ln ~/.zsh/zshrc ~/.zshrc
 
