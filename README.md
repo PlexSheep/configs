4306ed076e0f4874a26138b1360b05ec1fc0004b
PlexSheep <PlexSheep@protonmail.com>
2023-01-23 23:35:37 +0100

    added some cool stuff

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # configs
+
 some config files i use for my systems.
 you need to install fzf for some zsh stuff
 you also need vim plugged for vim plugins
--- desktop-config.sh
+++ desktop-config.sh
@@ -21,3 +21,32 @@ ln ~/.zsh/zshrc ~/.zshrc
 
 #cp -r $(pwd)/etc/* /etc
 #cp -r $(pwd)/usr/* /usr
+
+# Install Vim Stuff
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
+sudo bash -c 'rm ~/.zsh-very-old'
+sudo bash -c 'mv ~/.zsh-old ~/.zsh-very-old'
+sudo bash -c 'mv ~/.zsh ~/.zsh-old'
+
+sudo bash -c 'rm ~/.zshrc-very-old'
+sudo bash -c 'mv ~/.zshrc-old ~/.zshrc-very-old'
+sudo bash -c 'mv ~/.zshrc ~/.zshrc-old'
+
+sudo bash -c 'cp -r $(pwd)/home/.gitconfig ~'
+sudo bash -c 'cp -r $(pwd)/home/.p10k.zsh ~'
+sudo bash -c 'cp -r $(pwd)/home/.zsh ~'
+sudo bash -c 'cp -r $(pwd)/home/.vimrc ~'
+sudo bash -c 'cp -r $(pwd)/home/.local ~'
+sudo bash -c 'cp -r $(pwd)/home/.config ~'
+sudo bash -c 'cp -r $(pwd)/home/.tmux.conf ~'
+
+sudo bash -c 'ln ~/.zsh/zshrc ~/.zshrc'
+
+# Install Vim Stuff
+sudo sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
