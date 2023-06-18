9e44341ec0543aa5ae1627d100ecd6c06652f9f4
PlexSheep <software@cscherr.de>
2023-06-18 15:33:11 +0200

    nvim appimage installer

---
+++ nvim-appimage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p /tmp/nvim-install-appimage
+cd /tmp/nvim-install-appimage
+wget 'https://github.com/neovim/neovim/releases/download/stable/nvim.appimage'
+chmod +x nvim.appimage
+cp nvim.appimage /usr/local/bin
+
+# install alternatives
+CUSTOM_NVIM_PATH=/usr/local/bin/nvim.appimage
+#update-alternatives --remove ex "${CUSTOM_NVIM_PATH}"
+#update-alternatives --remove vi "${CUSTOM_NVIM_PATH}"
+#update-alternatives --remove view "${CUSTOM_NVIM_PATH}"
+#update-alternatives --remove vim "${CUSTOM_NVIM_PATH}"
+#update-alternatives --remove vimdiff "${CUSTOM_NVIM_PATH}"
+update-alternatives --install /usr/bin/nvim nvim "${CUSTOM_NVIM_PATH}" 110
