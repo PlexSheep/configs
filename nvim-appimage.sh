c9755478d9c1ffea67b812351acc5e0a6278c7b7
PlexSheep <software@cscherr.de>
2023-06-18 15:41:39 +0200

    auto install plug.vim with nvim

--- nvim-appimage.sh
+++ nvim-appimage.sh
@@ -14,3 +14,7 @@ CUSTOM_NVIM_PATH=/usr/local/bin/nvim.appimage
 #update-alternatives --remove vim "${CUSTOM_NVIM_PATH}"
 #update-alternatives --remove vimdiff "${CUSTOM_NVIM_PATH}"
 update-alternatives --install /usr/bin/nvim nvim "${CUSTOM_NVIM_PATH}" 110
+
+# get plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
